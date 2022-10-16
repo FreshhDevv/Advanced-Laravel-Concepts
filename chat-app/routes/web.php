0334e2d9e956d779eafcddc5adae7d6730266b5b
FreshDev <billyhans90@gmail.com>
2022-10-16 09:57:27 +0100

    Setup event and controller to send message

--- chat-app/routes/web.php
+++ chat-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::post('/send-message', [MessageController::class, 'send']);

7072f53eaeb955e1bf795bdca9650486df0994c0
FreshDev <billyhans90@gmail.com>
2022-11-23 22:12:46 +0100

    Dedicated class

--- view-composers/app/Providers/AppServiceProvider.php
+++ view-composers/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\View\Composers\ChannelsComposer;
 use App\Models\Channel;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -29,8 +30,11 @@ class AppServiceProvider extends ServiceProvider
         // View::share('channels', Channel::orderBy('name')->get());
 
         // Option 2 - Granular views with wildcards
-        View::composer(['post.*', 'channel.index'], function ($view) {
-            $view->with('channels', Channel::orderBy('name')->get());
-        });
+        // View::composer(['post.*', 'channel.index'], function ($view) {
+        //     $view->with('channels', Channel::orderBy('name')->get());
+        // });
+
+        // Option 3 - Dedicated Class
+        View::composer(['post.*', 'channel.index'], ChannelsComposer::class);
     }
 }

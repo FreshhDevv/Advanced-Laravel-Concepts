4ef20e32669f2b41b609053bdbc75a77bdaab060
FreshDev <billyhans90@gmail.com>
2022-11-24 12:11:37 +0100

    Minor refactoring

--- polymorphic-relationships/database/factories/PostFactory.php
+++ polymorphic-relationships/database/factories/PostFactory.php
@@ -2,13 +2,23 @@
 
 namespace Database\Factories;
 
-use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Post::class, function(Faker $faker) {
-    return [
-        'title' => $faker->sentence,
-        'body' => $faker->paragraph,
-
-    ];
-});
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentnce,
+            'body' => fake()->paragraph
+        ];
+    }
+}

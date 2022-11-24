2a994db019d14ce0598dc1535eb32b440c7bfecf
FreshDev <billyhans90@gmail.com>
2022-11-24 11:52:43 +0100

    Setup relationship from post to image

--- polymorphic-relationships/database/factories/PostFactory.php
+++ polymorphic-relationships/database/factories/PostFactory.php
@@ -2,24 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Fake
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
- */
-class PostFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(Faker $faker)
-    {
-        return [
-            'title' => $faker->sentnce,
-            'body' => $faker->paragraph
-        ];
-    }
-}
+$factory->define(Post::class, function(Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+
+    ];
+});

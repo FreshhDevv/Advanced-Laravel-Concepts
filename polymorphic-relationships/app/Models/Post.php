e55366a3ed79391b91d3f7d6c6ea0e21ad69d70a
FreshDev <billyhans90@gmail.com>
2022-11-26 13:20:28 +0100

    Many to many relationship establised

--- polymorphic-relationships/app/Models/Post.php
+++ polymorphic-relationships/app/Models/Post.php
@@ -10,12 +10,18 @@ class Post extends Model
     use HasFactory;
     protected $guarded = [];
 
-public function image() {
-    return $this->morphOne(Image::class, 'imageable')    ;
-}
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 
-public function comments() 
-{
-    return $this->morphToMany(Comment::class, 'commentable');
-}
+    public function comments()
+    {
+        return $this->morphToMany(Comment::class, 'commentable');
+    }
+
+    public function tags() 
+    {
+        return $this-> morphToMany(Tag::class, 'taggable');
+    }
 }

b2b3565d1274d5abe84759310e13b28ff36c0160
FreshDev <billyhans90@gmail.com>
2022-12-04 15:15:04 +0100

    Sort query filter don

--- pipelines/app/Http/Controllers/PostController.php
+++ pipelines/app/Http/Controllers/PostController.php
@@ -11,20 +11,13 @@ class PostController extends Controller
     public function index()
     {
 
-        $pipeline = app(Pipeline::class)->send(Post::query())->through([
+        $posts = app(Pipeline::class)->send(Post::query())->through([
             \App\QueryFilters\Active::class,
-        ])->thenReturn();
+            \App\QueryFilters\Sort::class,
+        ])->thenReturn()->get();
 
-        dd($pipeline->get());
 
-        if(request()->has('active')) {
-            $posts->where('active', request('active'));
-        }
-        
-        if(request()->has('sort')) {
-            $posts->orderBy('title', request('sort'));
-        }
-        $posts = $posts->get();
+       
         return view('post.index', compact('posts'));
     }
 }

9f8a448cacc59db32de4ca66265c2e0e16aff7da
FreshDev <billyhans90@gmail.com>
2022-12-04 15:00:44 +0100

    Active querty filter made

--- pipelines/app/Http/Controllers/PostController.php
+++ pipelines/app/Http/Controllers/PostController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Pipeline\Pipeline;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::query();
+
+        $pipeline = app(Pipeline::class)->send(Post::query())->through([
+            \App\QueryFilters\Active::class,
+        ])->thenReturn();
+
+        dd($pipeline->get());
 
         if(request()->has('active')) {
             $posts->where('active', request('active'));
         }
-
+        
+        if(request()->has('sort')) {
+            $posts->orderBy('title', request('sort'));
+        }
         $posts = $posts->get();
         return view('post.index', compact('posts'));
     }

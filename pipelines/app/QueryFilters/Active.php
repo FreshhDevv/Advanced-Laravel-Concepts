5d18deef2aac1765ea03c4674dc6421fd47399b1
FreshDev <billyhans90@gmail.com>
2022-12-05 16:27:32 +0100

    Query filtering working

--- pipelines/app/QueryFilters/Active.php
+++ pipelines/app/QueryFilters/Active.php
@@ -3,15 +3,12 @@
 namespace App\QueryFilters;
 use Closure;
 
-class Active
+class Active extends Filter
 {
-    public function handle($request, Closure $next)
-    {
-        if (! request()->has('active')) {
-            return $next($request);
-        }
-        $builder = $next($request);
+   
 
-        return $builder->where('active', request('active'));
+    protected function applyFilter($builder)
+    {
+        return $builder->where($this->filterName(), request($this->filterName()));
     }
 }
--- pipelines/app/QueryFilters/Sort.php
+++ pipelines/app/QueryFilters/Sort.php
@@ -1,17 +1,13 @@
 <?php
 
 namespace App\QueryFilters;
-use Closure;
 
-class Sort
+class Sort extends Filter
 {
-    public function handle($request, Closure $next)
-    {
-        if (!request()->has('sort')) {
-            return $next($request);
-        }
-        $builder = $next($request);
+    
 
-        return $builder->orderBy('title', request('sort'));
+    protected function applyFilter($builder)
+    {
+        return $builder->orderBy('title', request($this->filterName()));
     }
 }

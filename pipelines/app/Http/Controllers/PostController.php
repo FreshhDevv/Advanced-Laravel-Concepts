99cb226428737a0ba4e563a9a2fa74d2c85c9c7d
FreshDev <billyhans90@gmail.com>
2022-12-08 16:32:24 +0100

    Pipeline working

--- pipelines/app/Http/Controllers/PostController.php
+++ pipelines/app/Http/Controllers/PostController.php
@@ -10,11 +10,9 @@ class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::allPosts();
 
-        $posts = app(Pipeline::class)->send(Post::query())->through([
-            \App\QueryFilters\Active::class,
-            \App\QueryFilters\Sort::class,
-        ])->thenReturn()->get();
+        
 
 
        
--- pipelines/app/Models/Post.php
+++ pipelines/app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    public static function allPosts()
+    {
+        return
+            $posts = app(Pipeline::class)->send(Post::query())->through([
+                \App\QueryFilters\Active::class,
+                \App\QueryFilters\Sort::class,
+            ])->thenReturn()->get();
+    }
 }

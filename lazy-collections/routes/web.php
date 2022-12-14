029568a1ad6f105c444927e177a94e894ba4b0c9
FreshDev <billyhans90@gmail.com>
2022-12-14 18:39:11 +0100

    Demonstrating the problem lazy collections solves

--- lazy-collections/routes/web.php
+++ lazy-collections/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/lazy', function() {
+    $collection = Collection::times(1000000)
+    ->map(function($number) {
+    return pow(2, $number)        ;
+    })->all();
+
+    return 'done!';
 });
